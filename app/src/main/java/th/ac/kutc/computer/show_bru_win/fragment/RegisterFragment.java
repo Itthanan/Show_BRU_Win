73536e2b84ea7f865cdbf09a94ff8186c32fe035
Winnie <itthanan.su@gmail.com>
2018-04-25 16:28:14 +0700

    Replace Fragment Success

---
+++ app/src/main/java/th/ac/kutc/computer/show_bru_win/fragment/RegisterFragment.java
@@ -0,0 +1,22 @@
+package th.ac.kutc.computer.show_bru_win.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.ac.kutc.computer.show_bru_win.R;
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+
+    }
+}

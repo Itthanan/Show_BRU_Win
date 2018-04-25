6b61677ad50c8779dfc0d0a10abd4a492de241c2
Winnie <itthanan.su@gmail.com>
2018-04-25 14:11:01 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/kutc/computer/show_bru_win/fragment/MainFragment.java
@@ -0,0 +1,21 @@
+package th.ac.kutc.computer.show_bru_win.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.ac.kutc.computer.show_bru_win.R;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+}   // Main Class
